52a3363dd7d99710c69a485353f3d6ef018342a4
Yongsheng Xu <chuxdesign@hotmail.com>
2021-04-20 15:45:38 +0800

    feat: implement ui
    - server info
    - timer
    
    TODO:
    cbs

--- main.c
+++ main.c
@@ -10,33 +10,41 @@
 #define TRAY_ICON "icon.ico"
 
 static struct tray tray;
+static struct menu_context ctx;
 
 static void quit_cb(struct tray_menu *item) {
   (void)item;
   tray_exit();
 }
-
 static struct tray tray = {
     .icon = TRAY_ICON,
     .menu = (struct tray_menu[]){ {
-                         .text = "servers",
-                     },
-                     {.text = "-" },
-                     {.text = "quit", .cb = quit_cb },
-                     {.text = NULL } },
+                            .text = "servers",
+                        },
+                        {.text = "-" },
+                        {.text = "quit", .cb = quit_cb },
+                        {.text = NULL } },
 };
+
+static void timer_cb() {
+    update_submenu(&tray, &ctx);
+}
 
 int main() {
   if (tray_init(&tray) < 0) {
     printf("failed to create tray\n");
     return 1;
   }
+  HWND hWnd = GetConsoleWindow();
+  // ShowWindow(hWnd, SW_MINIMIZE);
+  ShowWindow(hWnd, SW_HIDE);
 
-  struct menu_context ctx;
   init_menu_context(&ctx, "127.0.0.1", 11080);
+  update_submenu(&tray, &ctx);
 
-  while (tray_loop(1) == 0) {
-  }
+  UINT_PTR timerid = SetTimer(NULL, 0, 30000, &timer_cb);
+
+  while (tray_loop(1) == 0) {}
 
   return 0;
 }
--- pegas_tray.h
+++ pegas_tray.h
@@ -3,7 +3,10 @@
 #include <winsock2.h>
 #include <Ws2tcpip.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "3rd-party/tray.h"
+
+#define DEFAULT_BUFLEN 2048
 
 struct menu_context {
 	struct pegas_control_endpoint {
@@ -11,6 +14,12 @@ struct menu_context {
 		int port;
 	} endpoint;
 	SOCKET sock;
+};
+
+struct server_info {
+	char title[256];
+	char desc[256];
+	struct server_info* next;
 };
 
 static void init_menu_context(struct menu_context * ctx, const char* host, int port) {
@@ -48,16 +57,101 @@ static void init_menu_context(struct menu_context * ctx, const char* host, int p
 
 	wprintf(L"Connected to server.\n");
 	ctx->sock = sock;
-	/*i_res = closesocket(sock);
-	if (i_res == SOCKET_ERROR) {
-		wprintf(L"closesocket function failed with error: %ld\n", WSAGetLastError());
-		WSACleanup();
-		exit(1);
-	}*/
-
-	//WSACleanup();
 }
 
-static void init_menu(struct tray_menu *root, struct menu_context * ctx) {
+static void update_submemu_by_info(struct tray_menu *root, const char * info) {
+	struct server_info* prev = root->context;
+	struct server_info* cur = prev->next;
+	const char s[2] = "\n";
+	char* token = NULL;
+	int server_len = 0;
+	do {
+		if(token == NULL)
+			token = strtok(info, s);
+		else
+			token = strtok(NULL, s);
+		if (token == NULL) break;
+		server_len += 1;
+		if (cur == NULL) {
+			cur = malloc(sizeof(struct server_info));
+			cur->next = NULL;
+		}
+		printf("title: %s\n", token);
+		sprintf(cur->title, "%s\n", token);
+		token = strtok(NULL, s);
+		printf("desc: %s\n", token);
+		sprintf(cur->desc, "%s\n", token);
+		prev->next = cur;
+		prev = cur;
+		cur = cur->next;
+	} while (1);
+	printf("server len: %d\n", server_len);
+
+	struct tray_menu* submenu = root->submenu;
+	if (submenu == NULL) {
+		// init submenu
+		submenu = malloc(3 * server_len * sizeof(struct tray_menu));
+	}
+
+	struct server_info* dummy =root->context;
+	struct server_info* sinfo = dummy->next;
+	for (int i = 0; i < server_len * 3; i += 3, sinfo = sinfo->next) {
+		if (sinfo != NULL) {
+			submenu[i].text = sinfo->title;
+			submenu[i].checked = sinfo->title[0] == '*';
+			submenu[i].disabled = 0;
+			submenu[i].cb = NULL; // TODO: pick server
+			submenu[i].submenu = NULL;
+			submenu[i].context = i / 3;
+
+			submenu[i + 1].text = sinfo->desc;
+			submenu[i + 1].disabled = 1;
+			submenu[i + 1].checked = 0;
+			submenu[i + 1].submenu = NULL;
+
+			submenu[i + 2].text = "-";
+			submenu[i + 2].submenu = NULL;
+		}
+	}
+	submenu[server_len * 3 - 1].text = NULL;
+	if (root->submenu == NULL)
+		root->submenu = submenu;
+}
+
+static void update_submenu(struct tray *tray, struct menu_context * ctx) {
+	if (tray->menu[0].context == NULL) {
+		struct server_info* sinfo = malloc(sizeof(struct server_info));
+		sinfo->next = NULL;
+		tray->menu[0].context = sinfo;
+	}
+	char* sendbuf = "get servers";
+	int i_res = send(ctx->sock, sendbuf, (int)strlen(sendbuf), 0);
+	if (i_res == SOCKET_ERROR) {
+		printf("send failed: %d\n", WSAGetLastError());
+		closesocket(ctx->sock);
+		WSACleanup();
+		return 1;
+	}
+	printf("Bytes Sent: %ld\n", i_res);
+
+	char recvbuf[DEFAULT_BUFLEN] = { 0 };
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	// blocking
+	i_res = recv(ctx->sock, recvbuf, recvbuflen, 0);
+	if (i_res > 0) {
+		if (i_res >= recvbuflen) {
+			printf("Not implement yet.\n");
+		}
+		else {
+			printf("Bytes received: %d\n%s\n", i_res, recvbuf);
+			update_submemu_by_info(&tray->menu[0], recvbuf);
+			tray_update(tray);
+		}
+	}
+	else if (i_res == 0)
+		printf("Connection closed\n");
+	else
+		printf("recv failed: %d\n", WSAGetLastError());
 
 }
